5caf5da3e57a8a4d63a5cdb1e1d7621b3cacaaa6
shahbaaz-halim <halimshahbaaz@gmal.com>
2026-02-08 23:45:06 +0530

    config done

---
+++ network_security/entity/config_entity.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from pathlib import Path
+from network_security.constant import training_pipeline
+
+print(training_pipeline.PIPELINE_NAME)
+print(training_pipeline.ARTIFACT_DIR)
+
+
+class TrainingPipelineConfig:
+    def __init__(self, timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name = training_pipeline.PIPELINE_NAME
+        self.artifact_name = training_pipeline.ARTIFACT_DIR
+        self.artifact_dir = Path(self.artifact_name) / timestamp
+        self.model_dir = Path("final_model")
+        self.timestamp: str = timestamp
+
+
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.data_ingestion_dir = (
+            training_pipeline_config.artifact_dir / training_pipeline.DATA_INGESTION_DIR_NAME
+        )
+        self.feature_store_file_path = (
+            self.data_ingestion_dir 
+            / training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR 
+            / training_pipeline.FILE_NAME
+        )
+        self.training_file_path = (
+            self.data_ingestion_dir 
+            / training_pipeline.DATA_INGESTION_INGESTED_DIR 
+            / training_pipeline.TRAIN_FILE_NAME
+        )
+        self.testing_file_path = (
+            self.data_ingestion_dir 
+            / training_pipeline.DATA_INGESTION_INGESTED_DIR 
+            / training_pipeline.TEST_FILE_NAME
+        )
+        self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+
+
+class DataValidationConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.data_validation_dir = (
+            training_pipeline_config.artifact_dir / training_pipeline.DATA_VALIDATION_DIR_NAME
+        )
+        self.valid_data_dir = (
+            self.data_validation_dir / training_pipeline.DATA_VALIDATION_VALID_DIR
+        )
+        self.invalid_data_dir = (
+            self.data_validation_dir / training_pipeline.DATA_VALIDATION_INVALID_DIR
+        )
+        self.valid_train_file_path = (
+            self.valid_data_dir / training_pipeline.TRAIN_FILE_NAME
+        )
+        self.valid_test_file_path = (
+            self.valid_data_dir / training_pipeline.TEST_FILE_NAME
+        )
+        self.invalid_train_file_path = (
+            self.invalid_data_dir / training_pipeline.TRAIN_FILE_NAME
+        )
+        self.invalid_test_file_path = (
+            self.invalid_data_dir / training_pipeline.TEST_FILE_NAME
+        )
+        self.drift_report_file_path = (
+            self.data_validation_dir
+            / training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR
+            / training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
+        )
+
+
+class DataTransformationConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.data_transformation_dir = (
+            training_pipeline_config.artifact_dir / training_pipeline.DATA_TRANSFORMATION_DIR_NAME
+        )
+        self.transformed_train_file_path = (
+            self.data_transformation_dir
+            / training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR
+            / training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy")
+        )
+        self.transformed_test_file_path = (
+            self.data_transformation_dir
+            / training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR
+            / training_pipeline.TEST_FILE_NAME.replace("csv", "npy")
+        )
+        self.transformed_object_file_path = (
+            self.data_transformation_dir
+            / training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR
+            / training_pipeline.PREPROCESSING_OBJECT_FILE_NAME
+        )
+
+
+class ModelTrainerConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.model_trainer_dir = (
+            training_pipeline_config.artifact_dir / training_pipeline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path = (
+            self.model_trainer_dir 
+            / training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR
+            / training_pipeline.MODEL_FILE_NAME
+        )
+        self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = (
+            training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
+        )
